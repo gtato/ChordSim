38c4cd74431e2cad302770700e9b5074f1609791
Genc <gtato6@gmail.com>
2017-01-09 16:48:02 +0100

    added libs

--- src/chord/ChordInitializer.java
+++ src/chord/ChordInitializer.java
@@ -43,7 +43,7 @@ public class ChordInitializer implements NodeInitializer, Control {
 		NodeComparator nc = new NodeComparator(pid);
 		Network.sort(nc);
 		myCreateFingerTable();
-		printNeighs();
+//		printNeighs();
 		return false;
 	}
 	
